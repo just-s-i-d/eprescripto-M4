16d13fb51a1abeb1ecbd9bfb43790ee077fe6617
Siddharth Paneri <dpaneri3@gmail.com>
2023-11-15 14:39:02 +0530

    feat:setup route for 404 page;removed index styles;

---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+import { Result, Button } from "antd";
+
+const PageNotFound = () => {
+  return (
+    <Result
+      status="404"
+      title="404"
+      subTitle="Sorry the page you visited,does not exist"
+      extra={
+        <Link to="/">
+          <Button type="primary">Home</Button>
+        </Link>
+      }
+    />
+  );
+};
+export default PageNotFound;
