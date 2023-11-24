f0005f2c521fba36c9f35a78ffb620d70a1e6865
Siddharth Paneri <dpaneri3@gmail.com>
2023-11-24 15:06:57 +0530

    feat:install tailwind;setup theme for antd;

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,32 @@
+import { useContext, useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import { ConfigProvider, Spin } from "antd";
 
 import "./App.css";
-import PageNotFound from "@pages/PageNotFound";
 import PrivateRoute from "@routes/PrivateRoute";
 import DashboardLayout from "@layouts/DashboardLayout";
+import { darkTheme, lightTheme } from "./theme/Theme";
+import { UserContext } from "@context/UserProvider";
+import RoleBasedRoutes from "@routes/RoleBasedRoutes";
 
 function App() {
+  const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const userContext = useContext(UserContext);
+  if (userContext === null) return <Spin />;
+  const { role } = userContext;
   return (
-    <Routes>
-      <Route path="/" element={<PrivateRoute />}>
-        <Route path="/dashboard" element={<DashboardLayout />} />
-      </Route>
-      <Route path="/*" element={<PageNotFound />} />
-    </Routes>
+    <ConfigProvider theme={isDarkTheme ? darkTheme : lightTheme}>
+      <Routes>
+        <Route path="/" element={<PrivateRoute />}>
+          <Route
+            path="/"
+            element={<DashboardLayout setIsDarkTheme={setIsDarkTheme} />}
+          >
+            <Route path="/*" element={<RoleBasedRoutes role={role} />} />
+          </Route>
+        </Route>
+      </Routes>
+    </ConfigProvider>
   );
 }
 
