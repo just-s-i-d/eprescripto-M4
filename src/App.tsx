999a16c885f1c3a847f843ac349984466045a0ae
Siddharth Paneri <dpaneri3@gmail.com>
2023-11-24 14:34:14 +0530

    feat:review changes;add user context;

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,13 @@ import { Routes, Route } from "react-router-dom";
 import "./App.css";
 import PageNotFound from "@pages/PageNotFound";
 import PrivateRoute from "@routes/PrivateRoute";
-import DoctorDashboardLayout from "@layouts/DoctorDashboardLayout";
+import DashboardLayout from "@layouts/DashboardLayout";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<PrivateRoute isLoggedIn />}>
-        <Route path="/dashboard" element={<DoctorDashboardLayout />} />
+      <Route path="/" element={<PrivateRoute />}>
+        <Route path="/dashboard" element={<DashboardLayout />} />
       </Route>
       <Route path="/*" element={<PageNotFound />} />
     </Routes>
--- src/routes/PrivateRoute.tsx
+++ src/routes/PrivateRoute.tsx
@@ -1,10 +1,21 @@
-import { Outlet, Navigate } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
 
-type props = {
-  isLoggedIn: boolean;
-};
-const PrivateRoute = ({ isLoggedIn }: props) => {
-  return isLoggedIn ? <Outlet /> : <Navigate to="/" />;
+import { UserContext } from "@context/UserProvider";
+import UnauthorisedPage from "@pages/UnauthorisedPage";
+import Loading from "@components/ui/Loading";
+
+const PrivateRoute = () => {
+  const userContext = useContext(UserContext);
+  const [loading, setLoading] = useState<boolean>(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+  }, []);
+  if (userContext === null) return <Loading />;
+  const { isLoggedIn } = userContext;
+  return loading ? <Loading /> : isLoggedIn ? <Outlet /> : <UnauthorisedPage />;
 };
 
 export default PrivateRoute;
