9fcc58bcf989412938621e77b4c8e4c9f31284b5
Siddharth Paneri <dpaneri3@gmail.com>
2023-12-04 14:34:14 +0530

    feat:review changes;

--- src/pages/DoctorDashboardPage.tsx
+++ src/pages/DoctorDashboardPage.tsx
@@ -1,6 +1,29 @@
-import DoctorDashboard from "@components/Doctor/DoctorDashboard";
+import { useEffect, useState } from "react";
 
-const DoctorDashboardPage = () => {
-  return <DoctorDashboard />;
+import InfoCards from "./InfoCards";
+
+import { InfoCardDetailsType } from "@constants/types";
+import { getInfoCardsData } from "@utils/Doctor";
+
+const DoctorDashboardPage: React.FC = () => {
+  const [infoCardError, setInfoCardError] = useState(false);
+  const [infoCardsData, setInfoCardsData] = useState<InfoCardDetailsType>();
+
+  useEffect(() => {
+    getInfoCardsData()
+      .then((res) => {
+        setInfoCardsData(res);
+        setInfoCardError(false);
+      })
+      .catch(() => setInfoCardError(true));
+  }, []);
+  return (
+    <div className="max-w-full flex">
+      <div className="w-full flex flex-col gap-6">
+        <InfoCards cardDetails={infoCardsData} error={infoCardError} />
+      </div>
+    </div>
+  );
 };
+
 export default DoctorDashboardPage;
