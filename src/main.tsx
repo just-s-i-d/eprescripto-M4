75ecb4231dda27525a3e110e48253f13bdc7d0d4
Siddharth Paneri <dpaneri3@gmail.com>
2023-12-04 18:01:23 +0530

    feat:add appointment page;add a custom hook;add types

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./index.css";
+import "./index.scss";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "@context/UserProvider.tsx";
+import ErrorBoundary from "@components/ErrorBoundary.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ErrorBoundary>
+          <App />
+        </ErrorBoundary>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
---
+++ src/pages/Doctor/AppointmentsPage.tsx
@@ -0,0 +1,143 @@
+import ErrorBoundary from "@components/ErrorBoundary";
+import UseStatesHook from "../../hooks/UseStatesHook";
+import { useEffect, useState } from "react";
+import {
+  cancelAppointment,
+  formatDateReadable,
+  getDoctorAppointmentsData,
+} from "@utils/Doctor";
+import {
+  AppointmentDataType,
+  DoctorAppointmentsDataType,
+} from "@constants/types";
+import TableCard from "@components/ui/TableCard";
+import PopModal from "@components/ui/PopModal";
+import { ColumnsType } from "antd/es/table";
+import { Card, Tooltip } from "antd";
+import { PhoneTwoTone, CloseCircleTwoTone } from "@ant-design/icons";
+import { toast } from "react-toastify";
+
+const DoctorAppointmentsPage = () => {
+  const appointmentsTable = UseStatesHook<DoctorAppointmentsDataType>();
+  const [open, setOpen] = useState(false);
+  const [selectedPatient, setSelectedPatient] = useState<AppointmentDataType>();
+  const handlerCancleAppointment = (patient: AppointmentDataType) => {
+    setOpen(true);
+    setSelectedPatient(patient);
+  };
+  const confirmHandler = async () => {
+    if (selectedPatient) {
+      const response = await cancelAppointment(selectedPatient);
+      response?.type === "success" && toast.success(response.message);
+      response?.type === "error" && toast.error(response.message);
+    }
+  };
+  useEffect(() => {
+    getDoctorAppointmentsData()
+      .then((res) => {
+        appointmentsTable.setData(res);
+        appointmentsTable.setError(false);
+        appointmentsTable.setLoading(false);
+      })
+      .catch(() => {
+        appointmentsTable.setError(true);
+        appointmentsTable.setLoading(false);
+      });
+  }, []);
+  const doctorAppointmentsTableColumns: ColumnsType<AppointmentDataType> = [
+    {
+      title: "Patient Id",
+      dataIndex: "pId",
+      width: 50,
+    },
+    {
+      title: "Patient Name",
+      dataIndex: "pName",
+      width: 150,
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      width: 200,
+    },
+    {
+      title: "Date",
+      dataIndex: "date",
+      width: 150,
+      render: (date, record) => (
+        <div className="flex flex-col">
+          <span>{formatDateReadable(date)}</span>
+          <span>{record.timeSlot}</span>
+        </div>
+      ),
+    },
+    {
+      title: "Referrer",
+      dataIndex: "referer",
+      width: 100,
+    },
+    {
+      title: "Status",
+      dataIndex: "status",
+      width: 50,
+      render: (value: string) => (
+        <span className={`py-1 px-2 rounded-lg ${value.toLowerCase()}`}>
+          {value}
+        </span>
+      ),
+      filters: [
+        { text: "Pending", value: "Pending" },
+        { text: "Attended", value: "Attended" },
+        { text: "Cancelled", value: "Cancelled" },
+      ],
+      onFilter: (value, record) => record.status === value,
+    },
+    {
+      title: "Action",
+      width: 100,
+      render: (_, record) => (
+        <div className="flex gap-4 text-lg">
+          <Tooltip title={record.contact}>
+            <PhoneTwoTone
+              className="rotate-90 cursor-pointer fill-[#19504c]"
+              href={`tel:${record.contact}`}
+            />
+          </Tooltip>
+          {record.status === "Pending" && (
+            <CloseCircleTwoTone
+              className="cursor-pointer"
+              onClick={() => handlerCancleAppointment(record)}
+            />
+          )}
+        </div>
+      ),
+    },
+  ];
+  return (
+    <>
+      <ErrorBoundary>
+        <Card className="appointment-table-card h-min w-full overflow-x-scroll">
+          <TableCard
+            className="min-w-[1100px]"
+            columns={doctorAppointmentsTableColumns}
+            table={appointmentsTable}
+            tableData={appointmentsTable.data}
+            pageSize={8}
+          />
+          <PopModal
+            setOpen={setOpen}
+            open={open}
+            okButtonText="Sure"
+            title="Cancel Appointment"
+            footer={true}
+            confirmHandler={confirmHandler}
+          >
+            Are you sure,you want to cancel the appoinment for{" "}
+            {selectedPatient?.pName}
+          </PopModal>
+        </Card>
+      </ErrorBoundary>
+    </>
+  );
+};
+export default DoctorAppointmentsPage;
