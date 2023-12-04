c899abee20d888aec9aefa5e1c018d44b7cd8326
Siddharth Paneri <dpaneri3@gmail.com>
2023-12-04 13:16:14 +0530

    feat:review changes;

--- src/App.tsx
+++ src/App.tsx
@@ -2,30 +2,36 @@ import { useContext, useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import { ConfigProvider, Spin } from "antd";
 
-import "./App.css";
 import PrivateRoute from "@routes/PrivateRoute";
 import DashboardLayout from "@layouts/DashboardLayout";
-import { darkTheme, lightTheme } from "./theme/Theme";
+import { darkTheme, lightTheme } from "./theme/theme.js";
 import { UserContext } from "@context/UserProvider";
 import RoleBasedRoutes from "@routes/RoleBasedRoutes";
+import "./App.css";
 
 function App() {
   const [isDarkTheme, setIsDarkTheme] = useState(false);
+  const [spinnng, setSpinning] = useState(false);
+  const [role, setRole] = useState("");
   const userContext = useContext(UserContext);
-  if (userContext === null) return <Spin />;
-  const { role } = userContext;
+
+  if (userContext === null) setSpinning(true);
+  else setRole(userContext.role);
+
   return (
     <ConfigProvider theme={isDarkTheme ? darkTheme : lightTheme}>
-      <Routes>
-        <Route path="/" element={<PrivateRoute />}>
-          <Route
-            path="/"
-            element={<DashboardLayout setIsDarkTheme={setIsDarkTheme} />}
-          >
-            <Route path="/*" element={<RoleBasedRoutes role={role} />} />
+      <Spin spinning={spinnng}>
+        <Routes>
+          <Route path="/" element={<PrivateRoute />}>
+            <Route
+              path="/"
+              element={<DashboardLayout setIsDarkTheme={setIsDarkTheme} />}
+            >
+              <Route path="/*" element={<RoleBasedRoutes role={role} />} />
+            </Route>
           </Route>
-        </Route>
-      </Routes>
+        </Routes>
+      </Spin>
     </ConfigProvider>
   );
 }
