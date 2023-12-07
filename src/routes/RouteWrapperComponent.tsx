44e104b492bbef5890461c7ef9c6b235da7c3f9a
Siddharth Paneri <dpaneri3@gmail.com>
2023-12-07 13:51:12 +0530

    feat:remove multiple return statement;

--- src/routes/RouteWrapperComponent.tsx
+++ src/routes/RouteWrapperComponent.tsx
@@ -1,15 +1,14 @@
+import { ReactElement, useContext } from "react";
+
 import Loading from "@components/ui/Loading";
 import { UserContext } from "@context/UserProvider";
-import { ReactElement, useContext } from "react";
 
 type RouteWrapperComponentProps = {
   components: { [key: string]: ReactElement };
 };
 const RouteWrapperComponent = ({ components }: RouteWrapperComponentProps) => {
   const userContext = useContext(UserContext);
-  if (userContext === null) return <Loading />;
-  const { role } = userContext;
-
-  return components?.[role];
+  const role = userContext?.role;
+  return role ? components?.[role] : <Loading />;
 };
 export default RouteWrapperComponent;
