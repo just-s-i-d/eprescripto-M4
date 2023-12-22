b4f97fd29ecaeee18eadf18988de1fc069225150
Siddharth Paneri <dpaneri3@gmail.com>
2023-12-22 15:06:53 +0530

    feat:review changes in guest route;

--- src/routes/GuestRoute.tsx
+++ src/routes/GuestRoute.tsx
@@ -1,12 +1,9 @@
-import Loading from "@components/ui/Loading";
 import { UserContext } from "@context/UserProvider";
 import { useContext } from "react";
 import { Navigate, Outlet } from "react-router-dom";
 
 const GuestRoute = () => {
-  const userContext = useContext(UserContext);
-  if (!userContext) return <Loading />;
-  const { isLoggedIn } = userContext;
+  const { isLoggedIn } = useContext(UserContext);
   return (
     <>
       {!isLoggedIn && <Outlet />}
